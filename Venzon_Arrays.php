15165a8c28dd11f7848d03234563c9a9240b042e
Jean <micjie23@gmail.com>
2025-12-05 00:21:48 +0800

    Update Venzon_Arrays.php
    update 2 for module 3

--- Venzon_Arrays.php
+++ Venzon_Arrays.php
@@ -14,6 +14,18 @@ $flowers = [
 ];
 
 $tax_rate = 12;
+
+function get_reorder_message(int $stock): string {
+    return ($stock < 10) ? "Yes" : "No";
+}
+
+function get_total_value(float $price, int $quantity): float {
+    return $price * $quantity;
+}
+
+function get_tax_due(float $price, int $quantity, int $tax_rate = 0): float {
+    return ($price * $quantity) * ($tax_rate / 100);
+}
 ?>
 
 <?php
