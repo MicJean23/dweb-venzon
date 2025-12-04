e4d945d399c01c158174e6d21881f436da09775b
Jean <micjie23@gmail.com>
2025-12-05 00:03:13 +0800

    Update Venzon_Arrays.php
    update 1 for module 3

--- Venzon_Arrays.php
+++ Venzon_Arrays.php
@@ -5,8 +5,15 @@ include "header.php";
 <h2>FLOWER BASKET</h2>
 
 <?php
+$flowers = [
+    "Tulips" => ["price" => 45.50, "stock" => 67],
+    "Roses" => ["price" => 30.00, "stock" => 8],
+    "Sunflowers" => ["price" => 25.75, "stock" => 15],
+    "Orchids" => ["price" => 120.00, "stock" => 3],
+    "Daisies" => ["price" => 18.00, "stock" => 22]
+];
 
-
+$tax_rate = 12;
 ?>
 
 <?php
